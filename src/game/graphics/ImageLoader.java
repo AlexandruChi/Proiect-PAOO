f2b277936969d1895e393b29b8f4ccf27c54b4f8
Alexandru Chiriac <alexandruchi203@icloud.com>
2023-05-09 08:11:43 +0300

    corners

--- src/game/graphics/ImageLoader.java
+++ src/game/graphics/ImageLoader.java
@@ -24,4 +24,40 @@ public class ImageLoader {
         AffineTransformOp flipOperation = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BICUBIC);
         return flipOperation.filter(originalImage, null);
     }
+
+    public static BufferedImage removeCorner(BufferedImage image, int corner) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage imageWithAlpha = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        imageWithAlpha.getGraphics().drawImage(image, 0, 0, null);
+
+        int startX = 0;
+        int startY = 0;
+        int endX = width;
+        int endY = height;
+
+        // TODO kill chatGPT and fix
+
+        if (corner == 1 || corner == 2) {
+            endX -= height;
+        }
+        if (corner == 2 || corner == 3) {
+            endY -= height;
+        }
+        if (corner == 3 || corner == 4) {
+            startY += height;
+        }
+        if (corner == 4 || corner == 1) {
+            startX += height;
+        }
+
+        for (int x = startX; x < endX; x++) {
+            for (int y = startY; y < endY; y++) {
+                imageWithAlpha.setRGB(x, y, imageWithAlpha.getRGB(x, y) & 0x00FFFFFF);
+            }
+        }
+
+        return imageWithAlpha;
+    }
 }
