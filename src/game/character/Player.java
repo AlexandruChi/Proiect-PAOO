76485ba16e6f0cc6d896a953435e94f200058f25
Alexandru Chiriac <alexandruchi203@icloud.com>
2023-05-29 22:36:14 +0300

    comentarii

--- src/game/character/Player.java
+++ src/game/character/Player.java
@@ -149,9 +149,7 @@ public class Player implements Character {
     public void addKill() {
         nrKills++;
         switch (nrKills) {
-            case 5, 10, 15, 25, 35, 45, 55, 65, 75 -> {
-                addMedal();
-            }
+            case 5, 10, 15, 20, 25, 30, 35, 40, 45 -> addMedal();
         }
         score += scoreForKill;
     }
